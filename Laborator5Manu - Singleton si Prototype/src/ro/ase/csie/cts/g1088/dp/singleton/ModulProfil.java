4edf504dd73ac087128228844b8b41ccd8799903
Visan Andreea Manuela <visanandreea18@stud.ase.ro>
2021-03-25 12:36:48 +0200

    Commit la pauza

---
+++ Laborator5Manu - Singleton si Prototype/src/ro/ase/csie/cts/g1088/dp/singleton/ModulProfil.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.g1088.dp.singleton;
+
+public class ModulProfil {
+	//ConexiuneBD conexiune = new ConexiuneBD("10.0.0.1", "superGame");
+	
+	ConexiuneBD conexiune = ConexiuneBD.getConexiune();
+}
